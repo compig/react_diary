0093ede698cf9f2bf1c405aa050e736dced52aa9
compig <44575059+compig@users.noreply.github.com>
2025-02-14 17:46:13 +0900

    feat: font, image, style

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,30 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 
+import emotion1 from "./assets/emotion1"
+import emotion2 from "./assets/emotion2"
+import emotion3 from "./assets/emotion3"
+import emotion4 from "./assets/emotion4"
+import emotion5 from "./assets/emotion5"
 function App() {
   return (
     <>
+    <div>
+      <img src={emotion1} />
+      <img src={emotion2} />
+      <img src={emotion3} />
+      <img src={emotion4} />
+      <img src={emotion5} />
+    </div>
+    <div>
+    <Link to={"/"}>Home</Link>
+    <Link to={"/new"}>New</Link>
+    <Link to={"/diary"}>Diary</Link>
+    </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary/>} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,5 +1,8 @@
+import { useParams } from "react-router-dom"
 const Diary = () => {
-    return <div>Diary</div>
+    const params = useParams();
+
+    return <div>Diary {params.id}</div>
 }
 
 export default Diary
