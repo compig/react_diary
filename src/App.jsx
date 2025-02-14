9e7cc74b4046ff228175112aff7cce2e6bc4e739
compig <44575059+compig@users.noreply.github.com>
2025-02-14 17:46:45 +0900

    feat: image module uill

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,27 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 
-import emotion1 from "./assets/emotion1"
-import emotion2 from "./assets/emotion2"
-import emotion3 from "./assets/emotion3"
-import emotion4 from "./assets/emotion4"
-import emotion5 from "./assets/emotion5"
+import { getEmotionImage } from "./util/get-emotion-image";
+
 function App() {
   return (
     <>
-    <div>
-      <img src={emotion1} />
-      <img src={emotion2} />
-      <img src={emotion3} />
-      <img src={emotion4} />
-      <img src={emotion5} />
-    </div>
-    <div>
-    <Link to={"/"}>Home</Link>
-    <Link to={"/new"}>New</Link>
-    <Link to={"/diary"}>Diary</Link>
-    </div>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary/>} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
