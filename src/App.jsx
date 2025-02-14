2c3db4c14dd31f4e7f767872831369e788be7eb4
compig <44575059+compig@users.noreply.github.com>
2025-02-14 20:58:01 +0900

    feat: diary create

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
 import "./App.css";
-import { useReducer } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useReducer, useRef } from "react";
+import { Routes, Route, data } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기",
+  },
+];
 
 function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+  }
   return state;
 }
 function App() {
-  const [data, dispatch] = useReducer(reducer, null);
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    })
+  }
+
   return (
     <>
+    <button onClick={()=>{
+      onCreate(new Date().getTime(), 1, "Hello")
+    }}>일기 추가 테스트</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
