2bc6d70ae24fe6c3af99f1042f83348595d43985
compig <44575059+compig@users.noreply.github.com>
2025-02-14 18:01:22 +0900

    feat: button component

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,29 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
-
-import { getEmotionImage } from "./util/get-emotion-image";
+import Button from "./components/Button"
 
 function App() {
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
+    <Button 
+    text={"123"}
+    type={"DEFAULT"}
+    onClick={()=> {
+    }}
+    />
+    <Button 
+    text={"123"}
+    type={"POSITIVE"}
+    onClick={()=> {
+    }}
+    />
+    <Button 
+    text={"123"}
+    type={"NEGATIVE"}
+    onClick={()=> {
+    }}
+    />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
