996b6ae470efdfd28c87d9234d67a0421e2a1a3e
compig <44575059+compig@users.noreply.github.com>
2025-02-15 21:41:04 +0900

    feat: header, diaryList, diaryDispatchContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,97 @@
-import "./App.css"
-import { useReducer, useRef, createContext } from "react"
-import { Routes, Route, data } from "react-router-dom"
-import Home from "./pages/Home"
-import New from "./pages/New"
-import Diary from "./pages/Diary"
-import Edit from "./pages/Edit"
-import Notfound from "./pages/Notfound"
+import './App.css'
+import { useReducer, useRef, createContext } from 'react'
+import { Routes, Route, data } from 'react-router-dom'
+import Home from './pages/Home'
+import New from './pages/New'
+import Diary from './pages/Diary'
+import Edit from './pages/Edit'
+import Notfound from './pages/Notfound'
 
 const mockData = [
   {
     id: 1,
     createdDate: new Date().getTime(),
     emotionId: 1,
-    content: "1번 일기",
+    content: '1번 일기',
   },
   {
     id: 2,
     createdDate: new Date().getTime(),
     emotionId: 2,
-    content: "2번 일기",
+    content: '2번 일기',
   },
-];
+]
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CREATE":
-      return [action.data, ...state];
-    case "UPDATE":
+    case 'CREATE':
+      return [action.data, ...state]
+    case 'UPDATE':
       return state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item
-      );
-    case "DELETE":
-      return state.filter((item) => String(item.id) !== String(action.id));
+      )
+    case 'DELETE':
+      return state.filter((item) => String(item.id) !== String(action.id))
     default:
       return state
   }
 }
 
-const DiaryStateContext = createContext();
+const DiaryStateContext = createContext()
 const DiaryDispatchContext = createContext()
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+  const [data, dispatch] = useReducer(reducer, mockData)
+  const idRef = useRef(3)
   const onCreate = (createdDate, emotionId, content) => {
     dispatch({
-      type: "CREATE",
+      type: 'CREATE',
       data: {
         id: idRef.current++,
         createdDate,
         emotionId,
         content,
       },
-    });
-  };
+    })
+  }
 
   const onUpdate = (id, createdDate, emotionId, content) =>
     dispatch({
-      type: "UPDATE",
+      type: 'UPDATE',
       data: {
         id,
         createdDate,
         emotionId,
         content,
       },
-    });
+    })
 
   const onDelete = (id) => {
     dispatch({
-      type: "DELETE",
+      type: 'DELETE',
       id,
     })
   }
 
   return (
     <>
-      <button
-        onClick={() => {
-          onCreate(new Date().getTime(), 1, "Hello")
-        }}
-      >
-        일기 추가 테스트
-      </button>
-      <button
-        onClick={() => {
-          onCreate(1, new Date().getTime(), 3, "수정된 일기")
-        }}
-      >
-        일기 수정 테스트
-      </button>
-      <button
-        onClick={() => {
-          onDelete(1)
-        }}
-      >
-        일기 삭제 테스트
-      </button>
       <DiaryStateContext.Provider value={data}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/diary/:id" element={<Diary />} />
-          <Route path="/edit/:id" element={<Edit />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
+        <DiaryDispatchContext.Provider value={(
+          onCreate,
+          onUpdate,
+          onDelete
+        )}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/diary/:id' element={<Diary />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            <Route path='*' element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
       </DiaryStateContext.Provider>
     </>
-  );
+  )
 }
 
 export default App
