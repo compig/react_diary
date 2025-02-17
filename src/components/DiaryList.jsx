274e96de259e2618b685ca671696160781c06af1
compig <44575059+compig@users.noreply.github.com>
2025-02-17 14:25:01 +0900

    feat: diaryList

--- src/components/DiaryList.jsx
+++ src/components/DiaryList.jsx
@@ -1,8 +1,9 @@
-import './DiaryList.css'
 import Button from './Button'
 import DiaryItem from './DiaryItem'
 
-const DiaryList = () => {
+import './DiaryList.css'
+
+const DiaryList = ({ data }) => {
   return (
     <div className='DiaryList'>
       <div className='menu_bar'>
@@ -10,10 +11,12 @@ const DiaryList = () => {
           <option value={'latest'}>최신순</option>
           <option value={'oldest'}>오래된 순</option>
         </select>
-        <Button text={'새 일기 쓰기'} type={"POSITIVE"} />
+        <Button text={'새 일기 쓰기'} type={'POSITIVE'} />
       </div>
       <div className='list_wrapper'>
-        <DiaryItem/>
+        {data.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   )
